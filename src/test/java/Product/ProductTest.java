1b595da1475f2ad37dff219c5fa24a6c11b66c69
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-20 10:35:42 -0400

    Refactored Product class testing for readability and to follow clean code (testing) best practices

--- src/test/java/Product/ProductTest.java
+++ src/test/java/Product/ProductTest.java
@@ -1,5 +1,6 @@
 package Product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,29 +8,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    Product p;
+    @BeforeEach
+    void setUp(){
+        p = new Product("Keyboards", 99.99, 3);
+    }
     @Test
-    @DisplayName("Checking the constructor's variable creation")
-    void checkInstanceKeyboard() {
-        var p = new Product("Keyboards", 99.99, 3);
+    @DisplayName("Checking the existence of variable used as constructor parameter: name")
+    void checkingExistenceName() {
         assertNotNull(p.getName());
+    }
+    @Test
+    @DisplayName("Checking the accuracy of variable used as constructor parameter: name")
+    void checkingAccuracyName() {
         assertEquals("Keyboards", p.getName());
-        assertNotNull(p.getCost());
+    }
+    @Test
+    @DisplayName("Checking the accuracy of variable used as constructor parameter: cost")
+    void checkingAccuracyCost() {
         assertEquals(99.99, p.getCost());
-        assertNotNull(p.getQuantity());
+    }
+    @Test
+    @DisplayName("Checking the accuracy of variable used as constructor parameter: quantity")
+    void checkingAccuracyQuantity() {
         assertEquals(3, p.getQuantity());
     }
     @Test
     @DisplayName("Constructor Case: Name created with zero-char")
     void checkInstanceName() {
         var n = new Product("", 99.99, 3);
-        assertNotNull(n.getName());
         assertEquals("", n.getName());
     }
     @Test
     @DisplayName("Constructor Case: Product cost is zero")
     void checkInstanceZeroCost() {
         var c = new Product("Keyboards", 0.00, 3);
-        assertNotNull(c.getCost());
         assertEquals(0.00, c.getCost());
+    }
+    @Test
+    @DisplayName("Constructor Case: Product quantity is zero")
+    void checkInstanceZeroQuantity() {
+        var c = new Product("Keyboards", 99.99, 0);
+        assertEquals(0.00, c.getQuantity());
     }
 }
