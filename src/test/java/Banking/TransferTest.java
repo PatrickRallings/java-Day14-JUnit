bcc48bac969695a1fff03d74016edfb8b3d0d4e9
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-20 09:27:38 -0400

    Refactored Transfer instance testing to utilize @BeforeEach

--- src/test/java/Banking/TransferTest.java
+++ src/test/java/Banking/TransferTest.java
@@ -1,14 +1,20 @@
 package Banking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransferTest {
-
-    BankAccount patrick = new BankAccount("Patrick", 5000.01);
-    BankAccount matt = new BankAccount("Matt", 1_000_000.69);
-    Transfer t = new Transfer(patrick, matt, 100);
+    BankAccount patrick;
+    BankAccount matt;
+    Transfer t;
+    @BeforeEach
+    void setUp(){
+        patrick = new BankAccount("Patrick", 5000.01);
+        matt = new BankAccount("Matt", 1_000_000.69);
+        t = new Transfer(patrick, matt, 100);
+    }
 
     @Test
     @DisplayName("Testing transfer instantiation")
