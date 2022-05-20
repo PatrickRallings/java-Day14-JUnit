adaa2d44f2cca10ff79871b17f2e6c8fef172996
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-20 11:55:42 -0400

    Added books package and corresponding class

---
+++ src/main/java/Books/Book.java
@@ -0,0 +1,49 @@
+package Books;
+
+public class Book {
+    public String title;
+    public Integer pagesCount;
+    public Integer yearPublished;
+
+    public Book(String title, Integer pagesCount, Integer yearPublished) {
+        this.title = title;
+        this.pagesCount = pagesCount;
+        this.yearPublished = yearPublished;
+    }
+    public Book(String title) {
+        this.title = title;
+    }
+    public Book(Integer pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+    public Book(String title, Integer yearPublished) {
+        this.title = title;
+        this.yearPublished = yearPublished;
+    }
+    public Book(Integer pagesCount, Integer yearPublished) {
+        this.pagesCount = pagesCount;
+        this.yearPublished = yearPublished;
+    }
+    public Book() {
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public Integer getPagesCount(){
+        return this.pagesCount;
+    }
+    public Integer getYearPublished(){
+        return this.yearPublished;
+    }
+    public String toString(String s){
+        String output = null;
+        if (s.equalsIgnoreCase("everything")){
+            output =  getTitle()+", "+getPagesCount()+" pages, "+getYearPublished();
+        } else if (s.equalsIgnoreCase("name")){
+            output = getTitle();
+        } else {
+            return "You did not enter 'Everything' or 'Name'.";
+        }
+        return output;
+    }
+}
