7fd15b6c434785c824b4eb5a63854d8c9d61aab7
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-20 09:21:02 -0400

    Refactored BankAccount instance testing to utilize @BeforeEach

--- src/test/java/Banking/BankAccountTest.java
+++ src/test/java/Banking/BankAccountTest.java
@@ -1,24 +1,28 @@
 package Banking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
 
+    BankAccount ba;
+    @BeforeEach
+    void setUp(){
+        ba = new BankAccount("Patrick", 69.69);
+    }
     @Test
     @DisplayName("Checking variable assignments from constructor methods are not null")
     void checkingVariablesExist(){
-        var ba = new BankAccount("Patrick", 69.69);
         assertNotNull(ba);
         assertNotNull(ba.getName());
-        assertNotNull(ba.getBalance());
     }
 
     @Test
     @DisplayName("Checking variable assignments from constructor methods are not accurate")
     void checkingVariablesAccuracy(){
-        var ba = new BankAccount("Patrick", 69.69);
         assertEquals("Patrick", ba.getName());
         assertEquals(69.69, ba.getBalance());
     }
